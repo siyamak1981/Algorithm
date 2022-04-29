63092e0d52098aaadc855c90ca13eac5bbefa330
siyamak1981@gmail.com <siyamak1981@gmail.com>
2022-04-29 17:07:09 +0430

    Pseudo Code Python

---
+++ listtodict.py
@@ -0,0 +1,16 @@
+# change list to dict
+x = [1,2,3,4,5,2,1,6,5]
+# print({i : x[i] for i in range(len(x))})
+
+
+# change list to dict with zip
+list = ['a', 1 , 'b', 5, 'c', 3, 'd', 99]
+def foo(arr):
+        it = iter(arr)
+        print(dict(zip(it, x)))
+
+foo(list)
+
+# arangment in dict
+# c = {'a':55, 'b':2, 'g':3, 'c': 4, 'c':4,'g':5, 'a':1}
+# print(c)
